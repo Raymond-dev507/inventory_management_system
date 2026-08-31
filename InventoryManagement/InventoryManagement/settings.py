1aae928e94a627ba8bd3404ad1f958e97facb365
unknown <lolyboy113@gmail.com>
2026-08-31 20:35:38 +0100

    Configure production allowed hosts

--- InventoryManagement/InventoryManagement/settings.py
+++ InventoryManagement/InventoryManagement/settings.py
@@ -42,10 +42,10 @@ if not DEBUG:
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-]
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS",
+    "localhost,127.0.0.1"
+).split(",")
 
 
 # Application definition
