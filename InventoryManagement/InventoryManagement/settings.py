3634ff42142ad490dd3c06073cb37d0464a8c4b9
unknown <lolyboy113@gmail.com>
2026-09-01 12:22:26 +0100

    Fix Gmail SMTP configuration

--- InventoryManagement/InventoryManagement/settings.py
+++ InventoryManagement/InventoryManagement/settings.py
@@ -181,7 +181,8 @@ LOGIN_URL = 'login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.getenv("EMAIL_ACCOUNT")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD")
